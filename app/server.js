5c86fb00957247e844c88277527146221b058ac1
c17an <kimchanmin1@gmail.com>
2021-07-06 19:48:24 +0900

    feat: 도커파일, 컴포즈 파일 완성

--- app/server.js
+++ app/server.js
@@ -1,23 +1,26 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+let MongoClient = require("mongodb").MongoClient;
+let bodyParser = require("body-parser");
 let app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/", function (req, res) {
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/profile-picture', function (req, res) {
+app.get("/profile-picture", function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+  res.writeHead(200, { "Content-Type": "image/jpg" });
+  res.end(img, "binary");
 });
 
 // use when starting application locally
@@ -32,29 +35,28 @@ let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
 let databaseName = "my-db";
 
-app.post('/update-profile', function (req, res) {
+app.post("/update-profile", function (req, res) {
   let userObj = req.body;
 
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
     let db = client.db(databaseName);
-    userObj['userid'] = 1;
+    userObj["userid"] = 1;
 
     let myquery = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+    db.collection("users").updateOne(myquery, newvalues, { upsert: true }, function (err, res) {
       if (err) throw err;
       client.close();
     });
-
   });
   // Send response
   res.send(userObj);
 });
 
-app.get('/get-profile', function (req, res) {
+app.get("/get-profile", function (req, res) {
   let response = {};
   // Connect to the db
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
